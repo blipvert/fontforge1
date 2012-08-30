332832601d6c6eb65e2446facbe98f7373603e9f
Craig Lennox <craig.lennox@avid.com>
2012-08-30 15:59:02 -0400

    More in-depth readline support

--- fontforge/scripting.c
+++ fontforge/scripting.c
@@ -42,6 +42,7 @@
 #ifdef HAVE_IEEEFP_H
 # include <ieeefp.h>		/* Solaris defines isnan in ieeefp rather than math.h */
 #endif
+#define USE_READLINE
 #ifdef USE_READLINE
 # include <readline/readline.h>
 # include <readline/history.h>
@@ -215,6 +216,10 @@ static void calldatafree(Context *c) {
 static void traceback(Context *c) {
     int cnt = 0;
     while ( c!=NULL ) {
+	if (c->interactive) {
+	    c->error = true;
+	    return;
+	}
 	if ( cnt==1 ) LogError( _("Called from...\n") );
 	if ( cnt>0 ) LogError( _(" %s: line %d\n"), c->filename, c->lineno );
 	calldatafree(c);
@@ -244,8 +249,12 @@ static void expect(Context *c,enum token_type expected, enum token_type got) {
     if ( got!=expected ) {
 	if ( verbose>0 )
 	    fflush(stdout);
-	LogError( _("%s: %d Expected %s, got %s"),
-		c->filename, c->lineno, toknames[expected], toknames[got] );
+	if (c->interactive)
+	    LogError( _("Error: Expected %s, got %s"),
+		toknames[expected], toknames[got] );
+	else
+	    LogError( _("%s: %d Expected %s, got %s"),
+		     c->filename, c->lineno, toknames[expected], toknames[got] );
 	if ( !no_windowing_ui ) {
 	    ff_post_error(NULL,_("%1$s: %2$d. Expected %3$s got %4$s"),
 		    c->filename, c->lineno, toknames[expected], toknames[got] );
@@ -257,8 +266,11 @@ static void expect(Context *c,enum token_type expected, enum token_type got) {
 static void unexpected(Context *c,enum token_type got) {
     if ( verbose>0 )
 	fflush(stdout);
-    LogError( _("%s: %d Unexpected %s found"),
-	    c->filename, c->lineno, toknames[got] );
+    if (c->interactive)
+	LogError( _("Error: Unexpected %s found"), toknames[got] );
+    else
+	LogError( _("%s: %d Unexpected %s found"),
+		 c->filename, c->lineno, toknames[got] );
     if ( !no_windowing_ui ) {
 	ff_post_error(NULL,"%1$s: %2$d Unexpected %3$",
 		c->filename, c->lineno, toknames[got] );
@@ -276,7 +288,9 @@ void ScriptError( Context *c, const char *msg ) {
 
     if ( verbose>0 )
 	fflush(stdout);
-    if ( c->lineno!=0 )
+    if ( c->interactive )
+	LogError( "Error: %s\n", t1 );
+    else if ( c->lineno!=0 )
 	LogError( _("%s line: %d %s\n"), ufile, c->lineno, t1 );
     else
 	LogError( "%s: %s\n", ufile, t1 );
@@ -294,6 +308,8 @@ void ScriptErrorString( Context *c, const char *msg, const char *name) {
 
     if ( verbose>0 )
 	fflush(stdout);
+    if ( c->interactive )
+	LogError( "Error: %s: %s\n", t1, t2 );
     if ( c->lineno!=0 )
 	LogError( _("%s line: %d %s: %s\n"), ufile, c->lineno, t1, t2 );
     else
@@ -317,7 +333,9 @@ void ScriptErrorF( Context *c, const char *format, ... ) {
     
     if ( verbose>0 )
 	fflush(stdout);
-    if ( c->lineno!=0 )
+    if (c->interactive)
+	LogError( _("Error: %s\n"), errbuf );
+    else if ( c->lineno!=0 )
 	LogError( _("%s line: %d %s\n"), ufile, c->lineno, errbuf );
     else
 	LogError( "%s: %s\n", ufile, errbuf );
@@ -8362,6 +8380,9 @@ static int __AddScriptLine(FILE *script, const char *line)
 	return -1;
 
     fputs(line, script);
+#ifdef USE_READLINE
+    fputs("\n\n", script);
+#endif
     fsetpos(script, &pos);
     return getc(script);
 }
@@ -8372,7 +8393,7 @@ static int __cgetc(Context *c) {
 
 	if ((ch = getc(c->script)) < 0) {
 #ifdef USE_READLINE
-	    char *line = readline(">> ");
+	    char *line = readline("> ");
 	    if (line) {
 		ch = __AddScriptLine(c->script, line);
 		add_history(line);
@@ -9826,6 +9847,17 @@ void ff_statement(Context *c) {
     } else {
 	ff_backuptok(c);
 	expr(c,&val);
+	if (c->interactive) {
+	    if (c->error) {
+		c->error = false;
+	    }
+	    else if (val.type != v_void) {
+		printf("-> ");
+		PrintVal(&val);
+		printf("\n");
+		fflush(stdout);
+	    }
+	}
 	if ( val.type == v_str )
 	    free( val.u.sval );
     }
